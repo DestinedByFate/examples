b10ed749304cd5c8f9366ede797a897d1b12ec34
DestinedByFate <maloy_bm@mail.ru>
2023-02-25 14:25:40 +0500

    add Example 005 Condition IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "булат")
+{
+    Console.WriteLine("Ура, это же Булат!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
