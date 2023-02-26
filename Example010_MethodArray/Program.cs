fa1d6dd71574eaea645c5091d3df59b4fcccf9a7
DestinedByFate <maloy_bm@mail.ru>
2023-02-26 23:28:12 +0500

    add example 010 method array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 25, 16, 31, 85, 48, 42, 16, 36, 48 };
+
+int n = array.Length;
+int find = 48;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
